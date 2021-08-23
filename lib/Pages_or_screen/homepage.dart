6b8282c7eaffb1a70d87de2fc3de73b4f683b0da
dattap647 <dattap647@gmail.com>
2021-08-23 23:43:34 +0530

    day 12

--- lib/Pages_or_screen/homepage.dart
+++ lib/Pages_or_screen/homepage.dart
@@ -1,25 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:run/Models/catalog.dart';
 import 'package:run/widget/drawer.dart';
+import 'package:run/widget/item_widgets.dart';
 
 class HomePage extends StatelessWidget {
-  
- int day = 30;
-  String name = "Datta";
+  final int days = 30;
+  final String name = "DAtta";
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Catalog App"),
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
-        body: Center(
-          child: Container(
-            child: Text("  Learnng flutter for  $day design better UI by $name " )
-          ),
-        ),
-        drawer: MyDrawer(),
-        
-      );
-      
-    
+      ),
+      drawer: MyDrawer(),
+    );
   }
 }
